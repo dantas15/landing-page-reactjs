8a008ad51e38ddc0ff3dcf31c049fe80a1ce0990
Gustavo Dantas Galote <gusgalote@outlook.com>
2022-02-21 12:58:33 -0300

    add GridSection w/ tests

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,42 @@
+import P from 'prop-types';
+
+import * as Styled from './styles';
+
+import { Heading } from 'components/Heading';
+import { SectionBackground } from 'components/SectionBackground';
+import { TextComponent } from 'components/TextComponent';
+
+export const GridText = ({ title, description, grid, background }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading size="huge" uppercase colorDark={!background} as="h2">
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>
+          {grid.map((element, index) => (
+            <Styled.GridElement key={index}>
+              <Heading size="medium" colorDark={!background} as="h3">
+                {element.title}
+              </Heading>
+              <TextComponent>{element.description}</TextComponent>
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridText.propTypes = {
+  background: P.bool,
+  title: P.string.isRequired,
+  description: P.string.isRequired,
+  grid: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      description: P.string.isRequired,
+    }),
+  ).isRequired,
+};
