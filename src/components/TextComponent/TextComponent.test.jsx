65d3f24579d102c44ff1fbdb1f1b51636680eef1
Gustavo Dantas Galote <gusgalote@outlook.com>
2022-02-21 16:00:29 -0300

    update TextComponent tag

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -17,11 +17,11 @@ describe('<TextComponent />', () => {
         font-size: 2.4rem;
       }
 
-      <p
+      <footer
         class="c0"
       >
         Children
-      </p>
+      </footer>
     `);
   });
 });
