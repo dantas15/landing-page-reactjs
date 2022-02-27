7ca14f004d44eb7d8634cf13c8dcaaa9eae6c7bc
Gustavo Dantas Galote <gusgalote@outlook.com>
2022-02-27 12:08:30 -0300

    fix random id

--- src/components/SectionBackground/index.jsx
+++ src/components/SectionBackground/index.jsx
@@ -4,18 +4,13 @@ import * as Styled from './styles';
 
 import { SectionContainer } from 'components/SectionContainer';
 
-const random = () =>
-  `id-${Math.random() * 10000}`.replace(/[^a-z0-9-_]/gi, '-');
-
 export const SectionBackground = ({
   children,
   background = false,
   sectionId = '',
 }) => {
-  const id = sectionId ? sectionId : random();
-
   return (
-    <Styled.Container background={background} id={id}>
+    <Styled.Container background={background} id={sectionId}>
       <SectionContainer>{children}</SectionContainer>
     </Styled.Container>
   );
